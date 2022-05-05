8a06dced3cb2a7ffc327d56bec6e1c156aae3966
Roman Grubic <grubic.roman@gmail.com>
2022-05-05 16:49:37 +0200

    Adding DataFixtures package and TaskFixtures.php file

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TaskFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $task = new Task();
+            $task->setName('Task number: ' . $i);
+            $task->setCreatedAt(new \DateTimeImmutable('now'));
+
+            $manager->persist($task);
+        }
+
+        $manager->flush();
+    }
+}
